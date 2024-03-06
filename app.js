fde0d746e2946d8f098e497259617e67bf99ec2b
Jennie <jguil429@gmail.com>
2024-03-06 13:39:49 -0600

    updated plant model

--- app.js
+++ app.js
@@ -21,17 +21,24 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get('/addplant', async (req, res) => {
-    const plant = new Plant({common_name: 'Pineland Hibiscus',
-        scientific_name: 'Hibiscus aculeatus',
-        native_location: "North Carolina to east Texas and Arkansas, mostly in coastal areas and along rivers.",
-        duration: 'Perennial',
-        edible: null,
-        // poisonous: Boolean,
-        description: 'Valued for its attractive flowers, this small-statured mallow blooms in summer and fall. Its flowers are cream colored with a dark red center, the petals appearing pleated with scalloped edges. In moist garden soil, it can live up to 6 years and attain a height of up to 6 bushy feet, though 3 feet is more common.'});
-    await plant.save();
-    res.send(plant);
+app.get('/plants', async(req, res) => {
+    const plants = await Plant.find({});
+    res.render('plants/index')
 });
+
+
+
+// app.get('/addplant', async (req, res) => {
+//     const plant = new Plant({common_name: 'Pineland Hibiscus',
+//         scientific_name: 'Hibiscus aculeatus',
+//         native_location: "North Carolina to east Texas and Arkansas, mostly in coastal areas and along rivers.",
+//         duration: 'Perennial',
+//         edible: null,
+//         // poisonous: Boolean,
+//         description: 'Valued for its attractive flowers, this small-statured mallow blooms in summer and fall. Its flowers are cream colored with a dark red center, the petals appearing pleated with scalloped edges. In moist garden soil, it can live up to 6 years and attain a height of up to 6 bushy feet, though 3 feet is more common.'});
+//     await plant.save();
+//     res.send(plant);
+// });
 
 app.listen(3000, () => {
     console.log('Port 3000')
--- models/plant.js
+++ models/plant.js
@@ -4,11 +4,10 @@ const Schema = mongoose.Schema;
 const PlantSchema = new Schema({
     common_name: String,
     scientific_name: String,
-    native_location: String,
+    native: Object,
     duration: String,
     edible: Boolean,
-    // poisonous: Boolean,
-    description: String
+    habit: Object
 });
 
 module.exports = mongoose.model('Plant', PlantSchema);
